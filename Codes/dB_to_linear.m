2ad23285c9b88822359d8eb1bc308c44786d59db
Mahamadou DANSOKO <165434666+MahaDeadHead23@users.noreply.github.com>
2025-11-01 10:37:14 +0100

    Delete Codes directory

--- Codes/dB_to_linear.m
+++
@@ -1,4 +0,0 @@
-function linear_val = dB_to_linear(dB_val)
-    % Converts a value from dB to linear scale.
-    linear_val = 10.^(dB_val / 10);
-end
--- Codes/linear_to_dB.m
+++
@@ -1,7 +0,0 @@
-function dB_val = linear_to_dB(linear_val)
-    if linear_val <= 1e-30 % Handle very small or non-positive inputs to avoid -Inf from log10
-        dB_val = -300; % Represents a very small dB value (e.g., -300 dB)
-    else
-        dB_val = 10 * log10(linear_val);
-    end
-end
